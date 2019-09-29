5ac8ed9dad388cbafba3fe59daecb6f7dd18b246
otumian <popecan1000@gmail.com>
2019-09-29 23:03:53 +0000

    using cdns

--- index.php
+++ index.php
@@ -5,16 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Php & Ajax</title>
-    <link rel="stylesheet" href="statics/bootstrap.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="statics/bootstrap.css"> -->
 </head>
 <body>
     <div class="container">
         <h1 class="bg-primary">Php and Ajax</h1>
         <p id="out_put" class="text-center"></p>
     </div>
-    
-    <script src="statics/jquery-3.4.1.js"></script>
-    <script src="statics/bootstrap.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- <script src="statics/jquery-3.4.1.js"></script> -->
+    <!-- <script src="statics/bootstrap.js"></script> -->
     <script>
         $(function() {
             $.ajax({
